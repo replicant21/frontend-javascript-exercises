915eca42da54674411b5792865cb7d6df1a8e796
Richard <argaen21@gmail.com>
2014-09-17 01:48:47 +0000

    Boolean challenge complete

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -11,9 +11,9 @@ module.exports.inBetween = function(lower, middle, upper) {
 };
 
 module.exports.outsideRanges = function(number) {
-	return ((number < 10 && number > 20) && (number <= 42 && number > 75) && (number <= 1 && number >=6));
+	return ( (number < 10 || number > 20) && (number <= 42 || number > 75) && (number <= 1 || number >=6));
 };
 
 module.exports.nameAndPrice = function(name, price) {
-	return ( (name === ("NYTimes" || "LATimes")) && ( price >= 1));
+	return ( (name === "NYTimes" || name === "LATimes") && (price >= 1) );
 };
