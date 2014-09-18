2ed47b4981947125e8a268b368341c298665df6b
Richard <argaen21@gmail.com>
2014-09-18 20:23:54 +0000

    Objects challenges complete

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,29 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+  var newObject = {
+    title : courseTitle,
+    duration : courseDuration,
+    students : courseStudents
+  };
+  return newObject;
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+  if (object[newProp] === undefined){
+    object[newProp] = newValue;
+  } 
+  return object;
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely," + "\n" + letter.sender;
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money){
+  var appleProducts = {
+    "MacBook Air" : 999,
+    "MacBook Pro" : 1299,
+    "Mac Pro"     : 2499,
+    "Apple Sticker" : 1
+  };
+  return ( (item === "MacBook Air" &&  money >= appleProducts["MacBook Air"]) || (item === "MacBook Pro" && money >= appleProducts["MacBook Pro"]) || (item === "Mac Pro" &&  money >= appleProducts["Mac Pro"]) || (item === "Apple Sticker" &&  money >= appleProducts["Apple Sticker"]) );
+}
