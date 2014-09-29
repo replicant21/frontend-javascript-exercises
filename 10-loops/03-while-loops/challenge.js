f1a72dc8e00e799e6c568d84e2edbec719b387fb
Richard <argaen21@gmail.com>
2014-09-29 23:21:24 +0000

    Loops Challenge completed*

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,22 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn){
+    actionFn = function() { console.log("Hello!"); };
+    counter = 10;
+      conditionalFn = function() { 
+        counter--;
+        return counter; 
+      };
+    while(counter > 0){
+      actionFn();
+      conditionalFn();
+    }
+};
 
-module.exports.sumNumbers = undefined;
-
+module.exports.sumNumbers = function(sumArray){
+  var i = 0;
+  var sum = 0;
+  while(i < sumArray.length){
+    sum = sum + sumArray[i];
+    i++;
+  }
+  return sum;
+};
